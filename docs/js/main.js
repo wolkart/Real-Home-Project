d4c330540137be002ade4ebb4962dbb1cb72710e
Wolkart <wolkov.art.leo@gmail.com>
2018-12-15 13:04:35 +0300

    First commit

---
+++ docs/js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+
+		// Вызов слайдера Owl-carousel
+		$("#top-slider").owlCarousel({
+			singleItem : true,
+			navigation: false,
+			theme: "top-slider-theme",
+			navigationText : ["",""],
+			slideSpeed : 600
+		});
+
+	
+});
