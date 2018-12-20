ac55c85672d3a4290fe1a4ec420ef989bcd6a908
Wolkart <wolkov.art.leo@gmail.com>
2018-12-21 02:45:56 +0300

    Final - adapted for mobile

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,29 +1,80 @@
 $(document).ready(function() {
 
-		// Вызов слайдера Owl-carousel
-		$("#top-slider").owlCarousel({
-			singleItem : true,
-			navigation: false,
-			theme: "top-slider-theme",
-			navigationText : ["",""],
-			slideSpeed : 600
-		});
+	// Скрипт для показа и скрытия выпадающего меню на смартфонах
+	    // Создаем переменые для кнопки и для меню
+	    var pull = $("#navigation-toggle");
+	    var nav = $(".navigation-list");
 
-		$("#agents-slider").owlCarousel({
-			singleItem : true,
-			navigation: false,
-			theme: "agents-slider-theme",
-			navigationText : ["",""],
-			slideSpeed : 600
-		});
+	    // Описываем событие при нажатии на кнопку
+	    $(pull).on("click", function(e){
+	    	
+        // Отменяем стандартное поведение ссылки в браузере
+        e.preventDefault();
 
-		$("#testimonials-slider").owlCarousel({
-			singleItem : true,
-			navigation: true,
-			theme: "testimonials-slider-theme",
-			navigationText : ["",""],
-			slideSpeed : 600
-		});
+        // Открываем/Скрываем меню
+        $(nav).slideToggle();
+
+		// Добавляем модификатор --active
+		$(this).toggleClass('navigation__toggle-button--active');
+
+	});
+
+
+    // При изменении размера окна, в большую сторону, если меню было скрыто, показываем его.
+    $(window).resize(function(){
+    	var w = $(window).width();
+    	if(w > 720 && nav.is(':hidden')) {
+    		nav.removeAttr('style');
+    	}
+    });
+
+	// Скрываем меню при клике на него на смартфоне и планцете
+	// По клику на ссылку в меню запускаем ф-ю fnstart();
+	$('.navigation-list__item').on("click", function(){
+		fnstart();
+	});
+
+	// В ф-ии fnstart(); проверяем - если меню открыто (проверяем по наличию класса --active у кнопки pull)
+	// тогда убираем класс модификатор --active у кнопки pull
+	// и сворачиваем/скрываем меню 
+	function fnstart(){	
+		if ( $("#navigation-toggle").hasClass("navigation__toggle-button--active")  ) {
+			pull.toggleClass('navigation__toggle-button--active');
+			nav.slideToggle();
+		}
+	};
+
+	
+
+	// Вызов слайдера Owl-carousel
+	$("#top-slider").owlCarousel({
+		singleItem : true,
+		navigation: false,
+		theme: "top-slider-theme",
+		navigationText : ["",""],
+		slideSpeed : 600
+	});
+
+	$("#agents-slider").owlCarousel({
+		singleItem : true,
+		navigation: false,
+		theme: "agents-slider-theme",
+		navigationText : ["",""],
+		slideSpeed : 600
+	});
+
+	$("#testimonials-slider").owlCarousel({
+		singleItem : true,
+		navigation: true,
+		theme: "testimonials-slider-theme",
+		navigationText : ["",""],
+		slideSpeed : 600
+	});
+
+	//slide2id - плавная прокрутка по ссылкам внутри страницы
+	$("nav a,a[href='#top'],a[rel='m_PageScroll2id'],a.PageScroll2id").mPageScroll2id({
+	    highlightSelector:"nav a"
+	});
 
 	
 });
